e5d6462dc7aa5052cc656694867fadbde2a9c3ad
yey3hh <wateveryehwant@gmail.com>
2025-09-29 15:25:28 +0900

    feat: 프로젝트 생성 및 기초 세팅 완료

---
+++ src/main/java/com/cocomoo/taily/TailyApplication.java
@@ -0,0 +1,13 @@
+package com.cocomoo.taily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TailyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cocomoo/taily/TailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocomoo.taily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
