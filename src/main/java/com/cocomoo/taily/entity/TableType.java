f446dd129cb9dee5ebb731713c6bade471d48d58
yey3hh <wateveryehwant@gmail.com>
2025-09-30 16:27:53 +0900

    feat: entity 생성

---
+++ src/main/java/com/cocomoo/taily/entity/TableType.java
@@ -0,0 +1,20 @@
+package com.cocomoo.taily.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "table_types")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class TableType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private TableType name;
+}
