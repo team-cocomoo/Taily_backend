fb5c6dd672d0f257938b6d26a9d9afd198bfa3d7
sangwook1199 <sangwook1199@gmail.com>
2025-10-13 00:07:21 +0900

    fix: 채팅 서비스 및 컨트롤러 수정

---
+++ src/main/java/com/cocomoo/taily/dto/chat/ChatRoomExistsResponseDto.java
@@ -0,0 +1,25 @@
+package com.cocomoo.taily.dto.chat;
+
+import com.cocomoo.taily.entity.MessageRoom;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatRoomExistsResponseDto {
+    private Long roomId;
+    private String user1Name;
+    private String user2Name;
+
+    public static ChatRoomExistsResponseDto from(MessageRoom room) {
+        return new ChatRoomExistsResponseDto(
+                room.getId(),
+                room.getUser1().getUsername(),
+                room.getUser2().getUsername()
+        );
+    }
+}
