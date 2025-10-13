b7a6570497123d7c16f847fa7b056f3dee5ca912
GIS <gogointospace@gmail.com>
2025-10-14 04:03:44 +0900

    chore : feed 작성 기능 구현 중

--- src/main/java/com/cocomoo/taily/controller/FeedController.java
+++ src/main/java/com/cocomoo/taily/controller/FeedController.java
@@ -2,6 +2,7 @@ package com.cocomoo.taily.controller;
 
 import com.cocomoo.taily.dto.petstory.FeedRequestDto;
 import com.cocomoo.taily.dto.petstory.FeedResponseDto;
+import com.cocomoo.taily.security.user.CustomUserDetails;
 import com.cocomoo.taily.service.FeedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,23 +19,35 @@ public class FeedController {
 
     private final FeedService feedService;
 
+    // 새로운 피드 등록 메서드
+    // Spring Seucurity에서 현재 로그인 유저의 id 가져오기
     @PostMapping
     public ResponseEntity<FeedResponseDto> createFeed(
-            @AuthenticationPrincipal UserDetails user,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            // @RequestBody FeedRequestDto dto, // JSON으로 받음
             @RequestPart("feed") FeedRequestDto dto,
             @RequestPart(value = "images", required = false) List<MultipartFile> images
     ) {
-        dto.setImages(images);
-        Long userId = Long.parseLong(user.getUsername());
+        dto.setImages(images); // images가 null일 수도 있음
+        Long userId = userDetails.getUserId();
         return ResponseEntity.ok(feedService.createFeed(userId, dto));
     }
 
+    // Feed id를 기준으로 feed 객체 반환
     @GetMapping("/{id}")
     public ResponseEntity<FeedResponseDto> getFeed(@PathVariable Long id) {
         return ResponseEntity.ok(feedService.getFeed(id));
     }
+    
+    // 전체 피드 조회
+    @GetMapping
+    public ResponseEntity<List<FeedResponseDto>> getAllFeeds() {
+        List<FeedResponseDto> feeds = feedService.getAllFeeds();
+        return ResponseEntity.ok(feeds);
+    }
 
-    @DeleteMapping("/{id}")
+    // 피드 삭제 메서드
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteFeed(
             @PathVariable Long id,
             @AuthenticationPrincipal UserDetails user
