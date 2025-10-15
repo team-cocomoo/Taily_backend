e1bdb699470ab3e0bed06c1cbd6e900daaa1762a
yey3hh <wateveryehwant@gmail.com>
2025-10-15 15:49:57 +0900

    fix: 내 좋아요 리스트 게시판마다 하나씩 조회하도록 코드 수정

--- src/main/java/com/cocomoo/taily/service/MyPageService.java
+++ src/main/java/com/cocomoo/taily/service/MyPageService.java
@@ -5,9 +5,7 @@ import com.cocomoo.taily.entity.*;
 import com.cocomoo.taily.repository.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -166,90 +164,91 @@ public class MyPageService {
     }
 
 
-    public List<MyLikesResponseDto> getMyLikes(String username) {
-        log.info("=== 내 좋아요 리스트 조회 시작 ===");
+//    public List<MyLikesResponseDto> getMyLikes(String username) {
+//        log.info("=== 내 좋아요 리스트 조회 시작 ===");
+//
+//        List<Like> myLikes = myLikesRepository.findByUserUsernameAndStateOrderByIdDesc(username, true);
+//
+//        List<MyLikesResponseDto> result = new ArrayList<>();
+//
+//        // 테이블 별 id 분리
+//        Map<TableTypeCategory, List<Long>> idsByCategory = myLikes.stream()
+//                .collect(Collectors.groupingBy(
+//                        like -> like.getTableType().getCategory(),
+//                        Collectors.mapping(Like::getPostsId, Collectors.toList())
+//                ));
+//
+//        // taily friends
+//        List<Long> tailyFriendsIds = idsByCategory.getOrDefault(TableTypeCategory.TAILY_FRIENDS, Collections.emptyList());
+//        if (!tailyFriendsIds.isEmpty()) {
+//            List<TailyFriend> friends = tailyFriendRepository.findAllByIdIn(tailyFriendsIds);
+//            friends.forEach(f -> {
+//                Like like = myLikes.stream().filter(l -> l.getPostsId().equals(f.getId()))
+//                        .findFirst().orElse(null);
+//                if (like != null) {
+//                    result.add(
+//                            MyLikesResponseDto.builder()
+//                                    .likeId(like.getId())
+//                                    .postId(like.getPostsId())
+//                                    .username(like.getUser().getUsername())
+//                                    .tableTypeId(like.getTableType().getId())
+//                                    .tableTypeCategory(like.getTableType().getCategory().getDisplayName())
+//                                    .targetName(f.getUser().getUsername())
+//                                    .build());
+//                }
+//            });
+//        }
+//
+//        // Feed
+//        List<Long> feedIds = idsByCategory.getOrDefault(TableTypeCategory.FEEDS, Collections.emptyList());
+//        if (!feedIds.isEmpty()) {
+//            List<Feed> feeds = feedRepository.findAllByIdIn(feedIds);
+//            feeds.forEach(feed -> {
+//                Like like = myLikes.stream()
+//                        .filter(l -> l.getPostsId().equals(feed.getId()))
+//                        .findFirst()
+//                        .orElse(null);
+//                if (like != null) {
+//                    result.add(MyLikesResponseDto.builder()
+//                            .likeId(like.getId())
+//                            .postId(like.getPostsId())
+//                            .username(like.getUser().getUsername())
+//                            .tableTypeId(like.getTableType().getId())
+//                            .tableTypeCategory(like.getTableType().getCategory().getDisplayName())
+//                            .targetName(feed.getUser().getUsername())
+//                            .build()
+//                    );
+//                }
+//            });
+//        }
+//
+//        // walkPath
+//        List<Long> walkPathIds = idsByCategory.getOrDefault(TableTypeCategory.WALK_PATHS, Collections.emptyList());
+//        if (!walkPathIds.isEmpty()) {
+//            List<WalkPath> walks = walkPathRepository.findAllByIdIn(walkPathIds);
+//            walks.forEach(walkPath -> {
+//                Like like = myLikes.stream()
+//                        .filter(l -> l.getPostsId().equals(walkPath.getId()))
+//                        .findFirst()
+//                        .orElse(null);
+//                if (like != null) {
+//                    result.add(MyLikesResponseDto.builder()
+//                            .likeId(like.getId())
+//                            .postId(like.getPostsId())
+//                            .username(like.getUser().getUsername())
+//                            .tableTypeId(like.getTableType().getId())
+//                            .tableTypeCategory(like.getTableType().getCategory().getDisplayName())
+//                            .targetName(walkPath.getUser().getUsername())
+//                            .build());
+//                }
+//            });
+//        }
+//
+//        log.info("조회된 내 좋아요 리스트 수 : {}", myLikes.size());
+//
+//        return result;
+//    }
 
-        List<Like> myLikes = myLikesRepository.findByUserUsernameAndState(username, true);
-
-        List<MyLikesResponseDto> result = new ArrayList<>();
-
-        // 테이블 별 id 분리
-        Map<TableTypeCategory, List<Long>> idsByCategory = myLikes.stream()
-                .collect(Collectors.groupingBy(
-                        like -> like.getTableType().getCategory(),
-                        Collectors.mapping(Like::getPostsId, Collectors.toList())
-                ));
-
-        // taily friends
-        List<Long> tailyFriendsIds = idsByCategory.getOrDefault(TableTypeCategory.TAILY_FRIENDS, Collections.emptyList());
-        if (!tailyFriendsIds.isEmpty()) {
-            List<TailyFriend> friends = tailyFriendRepository.findAllByIdIn(tailyFriendsIds);
-            friends.forEach(f -> {
-                Like like = myLikes.stream().filter(l -> l.getPostsId().equals(f.getId()))
-                        .findFirst().orElse(null);
-                if (like != null) {
-                    result.add(
-                            MyLikesResponseDto.builder()
-                                    .likeId(like.getId())
-                                    .postId(like.getPostsId())
-                                    .username(like.getUser().getUsername())
-                                    .tableTypeId(like.getTableType().getId())
-                                    .tableTypeCategory(like.getTableType().getCategory().name())
-                                    .targetName(f.getUser().getUsername())
-                                    .build());
-                }
-            });
-        }
-
-        // Feed
-        List<Long> feedIds = idsByCategory.getOrDefault(TableTypeCategory.FEEDS, Collections.emptyList());
-        if (!feedIds.isEmpty()) {
-            List<Feed> feeds = feedRepository.findAllByIdIn(feedIds);
-            feeds.forEach(feed -> {
-                Like like = myLikes.stream()
-                        .filter(l -> l.getPostsId().equals(feed.getId()))
-                        .findFirst()
-                        .orElse(null);
-                if (like != null) {
-                    result.add(MyLikesResponseDto.builder()
-                            .likeId(like.getId())
-                            .postId(like.getPostsId())
-                            .username(like.getUser().getUsername())
-                            .tableTypeId(like.getTableType().getId())
-                            .tableTypeCategory(like.getTableType().getCategory().name())
-                            .targetName(feed.getUser().getUsername())
-                            .build()
-                    );
-                }
-            });
-        }
-
-        // walkPath
-        List<Long> walkPathIds = idsByCategory.getOrDefault(TableTypeCategory.WALK_PATHS, Collections.emptyList());
-        if (!walkPathIds.isEmpty()) {
-            List<WalkPath> walks = walkPathRepository.findAllByIdIn(walkPathIds);
-            walks.forEach(walkPath -> {
-                Like like = myLikes.stream()
-                        .filter(l -> l.getPostsId().equals(walkPath.getId()))
-                        .findFirst()
-                        .orElse(null);
-                if (like != null) {
-                    result.add(MyLikesResponseDto.builder()
-                            .likeId(like.getId())
-                            .postId(like.getPostsId())
-                            .username(like.getUser().getUsername())
-                            .tableTypeId(like.getTableType().getId())
-                            .tableTypeCategory(like.getTableType().getCategory().name())
-                            .targetName(walkPath.getUser().getUsername())
-                            .build());
-                }
-            });
-        }
-
-        log.info("조회된 내 좋아요 리스트 수 : {}", myLikes.size());
-
-        return result;
-    }
     @Transactional
     public boolean toggleLike(String username, Long postsId, Long tableTypeId) {
         User user = userRepository.findByUsername(username).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
@@ -269,4 +268,90 @@ public class MyPageService {
         return like.isState();  // 토글 후 상태 반환
     }
 
+    public MyLikesPageResponseDto getMyLikesPage(String username, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id"));
+        Page<Like> likesPage = myLikesRepository.findByUserUsernameAndStateOrderByIdDesc(username, true, pageable);
+
+        List<Like> myLikes = likesPage.getContent();    // 실제 페이지 내용
+
+        // 테이블 별 id 분리
+        Map<TableTypeCategory, List<Long>> idsByCategory = myLikes.stream()
+                .collect(Collectors.groupingBy(
+                        like -> like.getTableType().getCategory(),
+                        Collectors.mapping(Like::getPostsId, Collectors.toList())
+                ));
+
+        List<MyLikesResponseDto> result = new ArrayList<>();
+
+        // 카테고리결 게시글 한 번만 조회 후 map 생성
+        Map<Long, TailyFriend> tailyFriendMap = tailyFriendRepository.findAllByIdIn(
+                idsByCategory.getOrDefault(TableTypeCategory.TAILY_FRIENDS, Collections.emptyList())
+        ).stream().collect(Collectors.toMap(TailyFriend::getId, tailyFriend -> tailyFriend));
+
+        Map<Long, Feed> feedMap = feedRepository.findAllByIdIn(
+                idsByCategory.getOrDefault(TableTypeCategory.FEEDS, Collections.emptyList())
+        ).stream().collect(Collectors.toMap(Feed::getId, feed -> feed));
+
+        Map<Long, WalkPath> walkMap = walkPathRepository.findAllByIdIn(
+                idsByCategory.getOrDefault(TableTypeCategory.WALK_PATHS, Collections.emptyList())
+        ).stream().collect(Collectors.toMap(WalkPath::getId, walkPath -> walkPath));
+
+
+        // 좋아요 + 게시글 매핑
+        for (Like like : myLikes) {
+            TableTypeCategory category = like.getTableType().getCategory();
+            switch (category) {
+                case TAILY_FRIENDS -> {
+                    TailyFriend tf = tailyFriendMap.get(like.getPostsId());
+                    if (tf != null) {
+                        result.add(MyLikesResponseDto.builder()
+                                .likeId(like.getId())
+                                .postId(like.getPostsId())
+                                .username(like.getUser().getUsername())
+                                .tableTypeId(like.getTableType().getId())
+                                .tableTypeCategory(category.getDisplayName())
+                                .targetName(tf.getUser().getUsername())
+                                .build());
+                    }
+                }
+                case FEEDS -> {
+                    Feed f = feedMap.get(like.getPostsId());
+                    if (f != null) {
+                        result.add(MyLikesResponseDto.builder()
+                                .likeId(like.getId())
+                                .postId(like.getPostsId())
+                                .username(like.getUser().getUsername())
+                                .tableTypeId(like.getTableType().getId())
+                                .tableTypeCategory(category.getDisplayName())
+                                .targetName(f.getUser().getUsername())
+                                .build());
+                    }
+                }
+                case WALK_PATHS -> {
+                    WalkPath w = walkMap.get(like.getPostsId());
+                    if (w != null) {
+                        result.add(MyLikesResponseDto.builder()
+                                .likeId(like.getId())
+                                .postId(like.getPostsId())
+                                .username(like.getUser().getUsername())
+                                .tableTypeId(like.getTableType().getId())
+                                .tableTypeCategory(category.getDisplayName())
+                                .targetName(w.getUser().getUsername())
+                                .build());
+                    }
+                }
+            }
+        }
+
+        log.info("조회된 내 좋아요 리스트 수 : {}", myLikes.size());
+
+        return MyLikesPageResponseDto.builder()
+                .myLikeList(result)
+                .page(page)
+                .size(size)
+                .totalCount(likesPage.getTotalElements())
+                .totalPages(likesPage.getTotalPages())
+                .isLast(likesPage.isLast())
+                .build();
+    }
 }
