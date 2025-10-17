d75a4639c416e232698213e27ed7afe00b9728bc
yey3hh <wateveryehwant@gmail.com>
2025-10-17 22:15:04 +0900

    fix: AlaramCategory 누락된 어노테이션 추가, 팔로잉 알람 기능 구현

--- src/main/java/com/cocomoo/taily/repository/TableTypeRepository.java
+++ src/main/java/com/cocomoo/taily/repository/TableTypeRepository.java
@@ -1,9 +1,13 @@
 package com.cocomoo.taily.repository;
 
 import com.cocomoo.taily.entity.TableType;
+import com.cocomoo.taily.entity.TableTypeCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TableTypeRepository extends JpaRepository <TableType, Long> {
+    Optional<TableType> findByCategory(TableTypeCategory tableTypeCategory);
 }
