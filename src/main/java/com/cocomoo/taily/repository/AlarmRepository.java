dae43f015995e31dd3cb47f278ce9331f54fe4de
yey3hh <wateveryehwant@gmail.com>
2025-10-19 17:52:32 +0900

    feat: 알람 읽음 처리 기능 구현, 기존 알람 리스트 출력 기능 구현

--- src/main/java/com/cocomoo/taily/repository/AlarmRepository.java
+++ src/main/java/com/cocomoo/taily/repository/AlarmRepository.java
@@ -1,9 +1,14 @@
 package com.cocomoo.taily.repository;
 
 import com.cocomoo.taily.entity.Alarm;
+import com.cocomoo.taily.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+    // 수신자 기준으로 최신순 정렬
+    List<Alarm> findByReceiverOrderByCreatedAtDesc(User user);
 }
