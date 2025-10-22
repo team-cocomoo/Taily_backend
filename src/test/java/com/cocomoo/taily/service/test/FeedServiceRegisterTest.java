7c3111b3d9964f436368dcad3e061db1f9dedc93
beomjunkim-soul <kbg142337@naver.com>
2025-10-22 15:00:38 +0900

    feat : mypage 구현

--- src/test/java/com/cocomoo/taily/service/test/FeedServiceRegisterTest.java
+++ src/test/java/com/cocomoo/taily/service/test/FeedServiceRegisterTest.java
@@ -9,6 +9,7 @@ import com.cocomoo.taily.security.config.SecurityConfig;
 import com.cocomoo.taily.service.FeedService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +24,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 @Slf4j
 @SpringBootTest
